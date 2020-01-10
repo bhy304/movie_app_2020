16de96a8387c7d5ba974c48baa50c6c7f790d57f
bhy304 <bhy0512@gmail.com>
2020-01-11 00:49:31 +0900

    Updates

---
+++ precache-manifest.243285dbee65b001c1e37351321c3b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f516be74ae990b5f68e77fa3f3caff",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0d23fb07d584c6242bd7",
+    "url": "/movie_app_2020/static/css/main.a9c6f193.chunk.css"
+  },
+  {
+    "revision": "5381da328ac8cae0861d",
+    "url": "/movie_app_2020/static/js/2.52032f24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.52032f24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d23fb07d584c6242bd7",
+    "url": "/movie_app_2020/static/js/main.d02d88e7.chunk.js"
+  },
+  {
+    "revision": "76a5fce50ff47ece0aac",
+    "url": "/movie_app_2020/static/js/runtime-main.67e4b511.js"
+  }
+]);
